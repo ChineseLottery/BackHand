20a7689c0183dd72ffa0afdb56cd3d401d0aef9c
Racheli-Kahana <leastudio6565@gmail.com>
2022-03-13 20:03:00 +0200

    addNewSchema

---
+++ Models/Wining.js
@@ -0,0 +1,6 @@
+const mongoose=require("mongoose");
+const WiningSchema=new mongoose.Schema({
+    userId:{type:String,match:/[0-9]{9}/},
+    itemId:Number,
+    winningDate:{type:Date,default:Date.now()}
+})
